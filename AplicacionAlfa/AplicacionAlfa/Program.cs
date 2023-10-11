53fe9cf5e7bfea7789d022a498708db77da267f6
2CB <mateojimenezfamaart@gmail.com>
2023-10-11 18:31:34 -0500

    Inicial Push

---
+++ AplicacionAlfa/AplicacionAlfa/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AplicacionAlfa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
